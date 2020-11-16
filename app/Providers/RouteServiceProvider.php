757a7479c0fc557f5435d30a9207ebd442e66b35
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-11-16 22:27:26 +0100

    Display answers for question
    Use edge load to display answer faster.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,7 +33,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         Route::bind('slug', function($slug){
-            $question = Question::where('slug', $slug)->first();
+            $question = Question::with('answers.user')->where('slug', $slug)->first();
             return $question ? $question : abort(404);
 
         });
