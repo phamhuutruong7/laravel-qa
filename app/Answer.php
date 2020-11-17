6b3a794c5ae20ec33405b83258deda632f3284cb
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-11-17 16:06:24 +0100

    Another option to remove best answer
    Another option to remove best answer. It may have error if datatype of best_answer_id and answer id is not suitable.

--- app/Answer.php
+++ app/Answer.php
@@ -32,13 +32,13 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $question = $answer->question;
-            $question->decrement('answers_count');
-            if( $question->best_answer_id === $answer->id)
-            {
-                $question->best_answer_id = NULL;
-                $question->save();
-            }
+//            $question = $answer->question;
+            $answer->question->decrement('answers_count');
+//            if( $question->best_answer_id === $answer->id)
+//            {
+//                $question->best_answer_id = NULL;
+//                $question->save();
+//            }
         });
     }
 
--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -10,13 +10,6 @@ use App\Question;
 class AnswersController extends Controller
 {
 
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Question $question, Request $request)
     {
         $question->answers()->create( $request->validate([
@@ -27,25 +20,13 @@ class AnswersController extends Controller
     }
 
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Question $question, Answer $answer)
     {
         $this->authorize('update', $answer);
         return view('answers.edit', compact('question', 'answer'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Question $question, Answer $answer)
     {
         $this->authorize('update', $answer);
@@ -55,12 +36,6 @@ class AnswersController extends Controller
         return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Question $question, Answer $answer)
     {
         $this->authorize('delete', $answer);
