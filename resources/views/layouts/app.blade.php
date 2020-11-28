8bcd803d86003a38afd2130cfa5fba0cd46b197f
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-11-28 01:36:06 +0100

    Rewrite favortie button with Vue
    Make favorite button with Vue.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -76,5 +75,14 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- Scripts -->
+    <script>
+        window.Auth = {!! json_encode([
+            'signedIn' => \Illuminate\Support\Facades\Auth::check(),
+            'user' => \Illuminate\Support\Facades\Auth::user(),
+        ]) !!}
+    </script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
