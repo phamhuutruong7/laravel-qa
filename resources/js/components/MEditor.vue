116fd201fe3136c1a1ca135682a570566340ed01
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-11-30 23:13:16 +0100

    Additional Commit
    Instead of using updated() method, we can use watch: {body:function(){ console.log('watch body'}} instead

--- resources/js/components/MEditor.vue
+++ resources/js/components/MEditor.vue
@@ -32,7 +32,7 @@ export default {
     },
     mounted(){
         autosize(this.$el.querySelector('textarea'))
-    },
+    }, 
     updated(){
         autosize(this.$el.querySelector('textarea'))
     }
