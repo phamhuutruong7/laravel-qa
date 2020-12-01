f5d2a79f1a66cee6d6bb14401a183c73b84a2cb5
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-12-01 01:07:16 +0100

    Add Markdown editor in Answer Component
    Add Markdown Editor in Answer Component

--- resources/js/components/Question.vue
+++ resources/js/components/Question.vue
@@ -10,7 +10,7 @@
                     <div class="media">
                         <div class="media-body">
                             <div class="form-group">
-                                <m-editor :body="body">
+                                <m-editor :body="body" :name="uniqueName">
                                     <textarea cols="30" rows="10" v-model="body" class="form-control" required></textarea>
                                 </m-editor>
                             </div>
@@ -53,20 +53,13 @@
     </div>
 </template>
 <script>
-import Vote from './Vote.vue';
-import UserInfo from './UserInfo.vue';
-import MEditor from './MEditor.vue';
+
 import modification from '../mixins/modification';
 
 
 export default{
     props:['question'],
     mixins:[modification],
-    components:{
-        Vote,
-        UserInfo,
-        MEditor,
-    },
     data(){
         return{
             title: this.question.title,
@@ -82,6 +75,9 @@ export default{
         },
         endpoint(){
             return `/questions/${this.id}`;
+        },
+        uniqueName(){
+            return `question-${this.id}`;
         }
     },
     methods:{
